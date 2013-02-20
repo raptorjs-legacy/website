ccc7053b6e94cfa78534c728c0d8efffe37657de
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-02-20 13:37:12 -0700

    Added a module for including code to support inspectlet (user tracking)

---
+++ components/tracking/Inspectlet/Inspectlet.js
@@ -0,0 +1,10 @@
+var __insp = __insp || [];
+__insp.push(['wid', 2568045892]);
+(function() {
+    function __ldinsp(){var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://www.inspectlet.com/inspectlet-jq.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); }
+    if (window.attachEvent){
+        window.attachEvent('onload', __ldinsp);
+    }else{
+        window.addEventListener('load', __ldinsp, false);
+    }
+})();
